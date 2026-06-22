3b914b49d89a94c8ec09a4b0adac54d8a8cc25c8
Joseph_Aguilar <148934634+Joseph321123@users.noreply.github.com>
2026-06-22 11:40:35 -0600

    feat(api): agregar detalle de propiedad por id (AIR-04) (#8)

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,6 +5,7 @@ const propertiesController = require('../controllers/propertiesController')
 const router = express.Router()
 
 router.get('/properties', propertiesController.listProperties)
+router.get('/properties/:id', propertiesController.getPropertyById)
 router.post('/auth/login', authController.login)
 
 module.exports = router
